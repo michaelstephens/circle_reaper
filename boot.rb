b9cd1fdd11cebd231caebb5ff6e9f4e74db6b2b3
Michael Stock <mikeastock@gmail.com>
2016-07-14 16:00:43 -0700

    Move config for Circle and Sidekiq to separate files

--- boot.rb
+++ boot.rb
@@ -3,9 +3,9 @@ lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require "bundler"
+
 Bundler.require
 
-require "dotenv"
 Dotenv.load
 
 # Load app
