afe55aa753e9270ac0046f5496f45cd26241d86e
Michael Stephens <michael.stephens@procore.com>
2016-07-15 12:00:07 -0700

    Add authentication option

--- lib/circle_reaper/app.rb
+++ lib/circle_reaper/app.rb
@@ -13,11 +13,21 @@ module CircleReaper
         object_class: HashWithIndifferentAccess
       )
 
-      commits = payload.fetch(:commits)
+      if ENV.fetch('SECRET_TOKEN')
+        request.body.rewind
+        payload_body = request.body.read
+        verify_signature(payload_body)
+      end
 
+      commits = payload.fetch(:commits)
       if commits.none? { |commit| commit.fetch(:message).include?("[run circle]") }
         CircleWorker.perform_in(30.seconds, payload)
       end
+    end
+
+    def verify_signature(payload_body)
+      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV.fetch('SECRET_TOKEN'), payload_body)
+      return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
     end
   end
 end
